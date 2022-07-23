2858cb65d6b45d67f7aa80d5fcf5206773300692
Daniel Vazquez <daniel.vazquez.vazquez@chem.ethz.ch>
2022-07-23 17:38:49 +0200

    added get_property method for material streams

--- pysis/flowsheet.py
+++ pysis/flowsheet.py
@@ -82,31 +82,74 @@ class MaterialStream:
         self.COMObject = COMObject
         self.comp_list = comp_list
         
-    def get_massflow(self, units = "kg/h") -> float:
+    def get_property(self, property_dict: dict) -> dict:
         """
-        Returns the total mass flow of the stream. The valid units are those that appear in HYSYS.
+        Specify a dictionary with different properties and the units you desire for them.
+        Get the properties returned in another dict. 
+        """
+        result_dict = {}
+        properties_not_found = []
+        for property in property_dict:
+            units = property_dict[property]
+            og_property = property
+            property = property.upper()
+            if property == "PRESSURE":
+                result_dict[og_property] = self.get_pressure(units)
+            elif property == "TEMPERATURE":
+                result_dict[og_property] = self.get_temperature(units)
+            elif property == "MASSFLOW" or property == "MASS_FLOW":
+                result_dict[og_property] = self.get_massflow(units)
+            elif property == "MOLARFLOW" or property == "MOLAR_FLOW":
+                result_dict[og_property] = self.get_molarflow(units)
+            elif property == "COMPMASSFLOW" or property == "COMPONENT_MASS_FLOW":
+                result_dict[og_property] = self.get_compmassflow(units)
+            elif property == "COMPMOLARFLOW" or property == "COMPONENT_MOLAR_FLOW":
+                result_dict[og_property] = self.get_compmolarflow(units)
+            elif property == "MASSFRACTION" or property == "MASS_FRACTION":
+                result_dict[og_property] = self.get_compmassfraction()
+            elif property == "MOLARFRACTION" or property == "MOLAR_FRACTION":
+                result_dict[og_property] = self.get_compmolarfraction()
+            else:
+                properties_not_found.append(og_property)
+        if properties_not_found:
+            print(f"WARNING. The following properties were not found: {properties_not_found}")
+        return result_dict
+            
+    def get_pressure(self, units = "bar"):
+        return self.COMObject.Pressure.GetValue(units)
+    
+    def set_pressure(self, value, units = "bar"):
+        assert self.COMObject.Pressure.State == 1, "The variable is calculated. Cannot modify it."
+        if value == "empty":
+            self.COMObject.Pressure.SetValue(-32767.0, "bar")
+        else:
+            self.COMObject.Pressure.SetValue(value, units)
+    
+    def get_temperature(self, units = "K"):
+        return self.COMObject.Temperature.GetValue(units)
+    
+    def set_temperature(self, value, units = "K"):
+        assert self.COMObject.Temperature.State == 1, "The variable is calculated. Cannot modify it."
+        if value == "empty":
+            self.COMObject.Temperature.SetValue(-32767.0, "K")
+        else: 
+            self.COMObject.Temperature.SetValue(value, units)
         
-        Input:
-            -units: String that indicates the units of the total mass flow.
-        """
+    def get_massflow(self, units = "kg/h") -> float:
         return self.COMObject.MassFlow.GetValue(units)
     
     def set_massflow(self, value: float, units:str = "kg/h") -> None:
+        assert self.COMObject.MassFlow.State == 1, "The variable is calculated. Cannot modify it."
         if value == "empty":
             self.COMObject.MassFlow.SetValue(-32767.0, "kg/h")
         else:
             self.COMObject.MassFlow.SetValue(value, units)
     
     def get_molarflow(self, units = "kgmole/h") -> float:
-        """
-        Returns the total molar flow of the stream. The valid units are those that appear in HYSYS.
-        
-        Input:
-            -units: String that indicates the units of the total molar flow.
-        """
-        return self.COMObject.Molar.GetValue(units)
+        return self.COMObject.MolarFlow.GetValue(units)
     
     def set_molarflow(self, value: float, units:str = "kgmole/h") -> None:
+        assert self.COMObject.MolarFlow.State == 1, "The variable is calculated. Cannot modify it."
         if value == "empty":
             self.COMObject.MolarFlow.SetValue(-32767.0, "kgmole/h")
         else:
@@ -116,6 +159,7 @@ class MaterialStream:
         return {i:j for (i,j) in zip(self.comp_list, self.COMObject.ComponentMassFlow.GetValues(units))}
     
     def set_compmassflow(self, values: dict, units = "kg/h"):
+        assert self.COMObject.ComponentMassFlow.State == 1, "The variable is calculated. Cannot modify it."
         values_to_set = [0]*len(self.comp_list)
         for component in values:
             values_to_set[self.comp_list.index(component)] = values[component]
@@ -126,6 +170,7 @@ class MaterialStream:
         return {i:j for (i,j) in zip(self.comp_list, self.COMObject.ComponentMolarFlow.GetValues(units))}
     
     def set_compmolarflow(self, values: dict, units = "kgmole/h"):
+        assert self.COMObject.ComponentMolarFlow.State == 1, "The variable is calculated. Cannot modify it."
         values_to_set = [0]*len(self.comp_list)
         for component in values:
             values_to_set[self.comp_list.index(component)] = values[component]
@@ -136,6 +181,7 @@ class MaterialStream:
         return {i:j for (i,j) in zip(self.comp_list, self.COMObject.ComponentMassFraction.GetValues(""))}
     
     def set_compmassfraction(self, values: dict):
+        assert self.COMObject.ComponentMassFraction.State == 1, "The variable is calculated. Cannot modify it."
         values_to_set = [0]*len(self.comp_list)
         for component in values:
             values_to_set[self.comp_list.index(component)] = values[component]
@@ -152,6 +198,12 @@ class MaterialStream:
         values_to_set = tuple(values_to_set)
         self.COMObject.ComponentMolarFraction.SetValues(values_to_set, "")
 
+    def get_connections(self):
+        upstream   = [i for i in self.COMObject.UpstreamOpers]
+        downstream = [i for i in self.COMObject.DownstreamOpers]
+        return {"Upstream": upstream, "Downstream": downstream}
+    
+    
 class EnergyStream:
     def __init__(self, COMObject):
         """"
